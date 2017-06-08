6da4c70b3f303b39fd871e8f398dbdba998a68e1
njad144 <njad144@aucklanduni.ac.nz>
2017-06-08 18:15:05 +1200

    Middle sync
    Middle sync - upto numeric value for y coordinate done.

--- helloUnityExample - Test2/Assets/Sketch.cs
+++ helloUnityExample - Test2/Assets/Sketch.cs
@@ -3,10 +3,8 @@ using Pathfinding.Serialization.JsonFx; //make sure you include this using
 
 public class Sketch : MonoBehaviour {
     public GameObject myPrefab;
-    //string DataSeat = "treesurveyv3"; 
-    // Put your URL here
-    //public 
-    string _WebsiteURL = "http://infomgmt192.azurewebsites.net/tables/" + "treesurveyv3" + "?zumo-api-version=2.0.0";
+	//njad144 URL points to specified table
+    string _WebsiteURL = "http://infomgmt192.azurewebsites.net/tables/" + "RevenueTest2" + "?zumo-api-version=2.0.0";
 
     void Start () {
         //Reguest.GET can be called passing in your ODATA url as a string in the form:
@@ -21,29 +19,29 @@ public class Sketch : MonoBehaviour {
             return;
         }
 
-        //We can now deserialize into an array of objects - in this case the class we created. The deserializer is smart enough to instantiate all the classes and populate the variables based on column name.
-        TreeSurvey[] treesurveys = JsonReader.Deserialize<TreeSurvey[]>(jsonResponse);
+		//njad144 Deserialize into array of revenues
+        RevenueTest2[] revenues = JsonReader.Deserialize<RevenueTest2[]>(jsonResponse);
 
-        int totalCubes = treesurveys.Length;
+        int totalCubes = revenues.Length;
         int totalDistance = 5;
         int i = 0;
-        //We can now loop through the array of objects and access each object individually
-        foreach (TreeSurvey treesurvey in treesurveys)
+
+		//njad144 Looping through all of the revenue objects to render new cubes.
+        foreach (RevenueTest2 revenue in revenues)
         {
-            //Example of how to use the object
-            Debug.Log("This products name is: " + treesurvey.Location);
             float perc = i / (float)totalCubes;
-            i++;
             float x = perc * totalDistance;
-            float y = treesurvey.Y;// 5.0f;
+			float y = 5.0f;//revenue.Y;// 5.0f;
             float z = 0.0f;
             GameObject newCube = (GameObject)Instantiate(myPrefab, new Vector3(x, y, z), Quaternion.identity);
             newCube.GetComponent<myCubeScript>().setSize((1.0f - perc) * 2);
             newCube.GetComponent<myCubeScript>().ratateSpeed = perc;
-            newCube.GetComponentInChildren<TextMesh>().text = treesurvey.Location;
+            newCube.GetComponentInChildren<TextMesh>().text = revenue.City;
             newCube.GetComponent<Renderer>().material.color = Color.green;
 
-        }
+
+			i++;
+		}
     }
 	
 	// Update is called once per frame
